95abab0195f57dce41862a10c16ec479b6f03691
Boutevin Clement <clement.boutevin@e-artsup.net>
2020-01-02 19:13:21 +0100

    Le player peut se déplacer et regarde vers la souris + j'ai mis un ptit pack de vfx pour faire jolie

---
+++ HotlineMiami/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 moveVelocity;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * moveSpeed;
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
