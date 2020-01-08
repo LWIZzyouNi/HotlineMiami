6d450b4144e95ad66532f39ce9e7bdfa42ffbb76
Boutevin Clement <clement.boutevin@e-artsup.net>
2020-01-08 11:27:45 +0100

    on peux désormais tirer vers ou on regarde et on se tape contre des blocs

--- HotlineMiami/Assets/Scripts/PlayerController.cs
+++ HotlineMiami/Assets/Scripts/PlayerController.cs
@@ -7,21 +7,47 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public Vector2 moveVelocity;
     public Rigidbody2D rb;
+    public bool isInLife;
+    public GameObject bullet;
+    Vector2 bulletPos;
+    public float fireRate = 0.0f;
+    float nextFire = 0.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isInLife = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = moveInput.normalized * moveSpeed;
+        if (isInLife == true)
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            moveVelocity = moveInput.normalized * moveSpeed;
+        }
+
+        if (Input.GetMouseButton(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Fire();
+        }
     }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
+
+
+    void Fire()
+    {
+        /*bulletPos = transform.position;
+        bulletPos += new Vector2(+1f, 0f);
+        Instantiate(bullet, bulletPos, Quaternion.identity);*/
+
+    }
+
 }
---
+++ HotlineMiami/Assets/justDestroyAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class justDestroyAfterTime : MonoBehaviour
+{
+    float timeToDestroy;
+    public float baseTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToDestroy = baseTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToDestroy -= Time.deltaTime;
+        
+        if (timeToDestroy <= 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
